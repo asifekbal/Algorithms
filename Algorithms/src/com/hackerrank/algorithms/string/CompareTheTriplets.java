c79f967bc78cd9d08428e16197f87500a1790101
Asif Ekbal <asif.sunny678@gmail.com>
2017-09-18 12:03:13 +0530

    changed

--- Algorithms/src/com/hackerrank/algorithms/string/CompareTheTriplets.java
+++ Algorithms/src/com/hackerrank/algorithms/string/CompareTheTriplets.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class CompareTheTriplets {
 	public static void main(String args[]) {
 		Scanner scr = new Scanner(System.in);
+		System.out.println("Hello");
 	}
 }
