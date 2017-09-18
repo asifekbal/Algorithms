73405713e0f921c40c29ec5268e2bbcc4b9c69f2
Asif Ekbal <asif.sunny678@gmail.com>
2017-09-18 12:01:36 +0530

    Initial commit

---
+++ Algorithms/src/com/hackerrank/algorithms/string/CompareTheTriplets.java
@@ -0,0 +1,9 @@
+package com.hackerrank.algorithms.string;
+
+import java.util.Scanner;
+
+public class CompareTheTriplets {
+	public static void main(String args[]) {
+		Scanner scr = new Scanner(System.in);
+	}
+}
